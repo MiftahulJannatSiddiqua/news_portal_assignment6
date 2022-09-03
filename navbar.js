49aa6fa17c262014204c142c503515e14c224eb1
MiftahulJannatSiddiqua <mjsmifta7@gmail.com>
2022-09-04 00:48:55 +0600

    commit added

--- navbar.js
+++ navbar.js
@@ -26,13 +26,23 @@ const loadNavBar=() =>{
     }
     
   const  dataStore= data =>{
+   // console.log(data)
     const navContainer = document.getElementById('chokinar-ma');
     console.log(data);
     data.forEach(storeTheData=>{
+        console.log(storeTheData);
         const newsDiv=document.createElement('div')
-        navPharagraph.innerHTML=`
-        <div id="green"></div> 
-        <div id="news-details"></div>
+        newsDiv.classList.add('displayFlex')
+       newsDiv.innerHTML=`
+        <div>
+       <img src="${storeTheData.thumbnail_url}">
+        </div>
+        <div>
+            <div><h3>${storeTheData.title}</h3><p>${storeTheData.details}</p></div>
+            <div class="flex-display"><div><img class="author-img" src="${storeTheData.author.img}"</div>
+           <div><h4>${storeTheData.author.name}</h4></div><div><span>${storeTheData.author.published_date}</span></div></div>
+        </div>
         `;
+        navContainer.appendChild(newsDiv);
     })
   }
